f5cf98517bfc34a87f7f2e10c9e7ccffe2d5bfa9
Марат Мелкумян <melkumyanmarat9@gmail.com>
2022-09-07 17:42:01 +0300

    2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
-from rest_framework.exceptions import PermissionDenied
+from .permissions import AuthorOrReadOnly
 
 from posts.models import Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-
-API_RAISE_403 = PermissionDenied('Изменение чужого контента запрещено!')
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -16,39 +14,20 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise API_RAISE_403
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise API_RAISE_403
-        instance.delete()
-
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        new_queryset = post.comments.all()
-        return new_queryset
+        return post.comments.all()
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise API_RAISE_403
-        super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise API_RAISE_403
-        instance.delete()
